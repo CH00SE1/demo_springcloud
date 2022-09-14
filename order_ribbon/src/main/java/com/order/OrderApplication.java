9de2b7e43cd473829a8561683a08b67146525d77
liushaoxiong <3354621952@qq.com>
2022-09-14 20:13:50 +0800

    整合ribbon负载均衡+重写负载均衡算法实现

--- order_ribbon/src/main/java/com/order/OrderApplication.java
+++ order_ribbon/src/main/java/com/order/OrderApplication.java
@@ -1,13 +1,19 @@
 package com.order;
 
+import com.order.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 /**
  * @author CHOOSE1
  * @date 2022-09-14 15:26:41
  */
 @SpringBootApplication
+@RibbonClients(value = {
+        @RibbonClient(name = "stock-service", configuration = RibbonConfig.class)
+})
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
---
+++ order_ribbon/src/main/java/com/order/config/RibbonConfig.java
@@ -0,0 +1,19 @@
+package com.order.config;
+
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author CHOOSE1
+ * @date 2022-09-14 19:11:51
+ */
+@Configuration
+public class RibbonConfig {
+
+    @Bean
+    public IRule iRule() {
+        return new CustomRule();
+    }
+
+}
