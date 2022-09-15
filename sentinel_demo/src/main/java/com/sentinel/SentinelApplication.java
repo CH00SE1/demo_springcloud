1a5aae726e7217861c1628a2d2a4b55eeb0282e4
liushaoxiong <3354621952@qq.com>
2022-09-15 14:31:54 +0800

    sentinel自定义流控2

--- sentinel_demo/src/main/java/com/sentinel/SentinelApplication.java
+++ sentinel_demo/src/main/java/com/sentinel/SentinelApplication.java
@@ -1,7 +1,9 @@
 package com.sentinel;
 
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author CHOOSE1
@@ -11,5 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SentinelApplication {
     public static void main(String[] args) {
         SpringApplication.run(SentinelApplication.class, args);
+    }
+
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
     }
 }
--- sentinel_demo/src/main/java/com/sentinel/controller/SentinelController.java
+++ sentinel_demo/src/main/java/com/sentinel/controller/SentinelController.java
@@ -3,6 +3,7 @@ package com.sentinel.controller;
 import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.Tracer;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
@@ -67,9 +68,43 @@ public class SentinelController {
         flowRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
         // 设置受保护的资源阈值
         flowRule.setCount(1);
-        rules.add(flowRule);
+        FlowRule flowRule2 = new FlowRule();
+        // 设置那个资源进行流控
+        flowRule2.setResource(USER_RESOUCE_NAME);
+        // 设置流控规则QPS
+        flowRule2.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // 设置受保护的资源阈值
+        flowRule2.setCount(1);
+        rules.add(flowRule2);
         // 加载配置规则
         FlowRuleManager.loadRules(rules);
+    }
+
+    /**
+     * @return
+     * @SentinelResource 改善接口中资源定义和被流控后降级的处理方法
+     * 1.添加sentinel切面依赖
+     * 2.配置bean SentinelResourceAspect
+     * 3.value 定义资源
+     * 4.blockHandler 流控降级后的处理方法 [默认该方法必须声明在同一个类中]
+     */
+    @GetMapping("/user")
+    @SentinelResource(value = USER_RESOUCE_NAME, blockHandler = "blockHandlerForString")
+    public String display(String id) {
+        return id + " - 正常";
+    }
+
+    /**
+     * 一定要是public
+     * 返回值以及参数和原方法保持一致
+     *
+     * @param id
+     * @param ex
+     * @return
+     */
+    public String blockHandlerForString(String id, BlockException ex) {
+        ex.printStackTrace();
+        return "流控!!!";
     }
 
 }
