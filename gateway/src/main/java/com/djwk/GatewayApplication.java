6b644a02d88e37fdb6f9ad6ecab98a908fcfbc39
liushaoxiong <3354621952@qq.com>
2022-10-13 21:00:56 +0800

    gateway整合nacos网关一阶段提交

---
+++ gateway/src/main/java/com/djwk/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.djwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CHOOSE1
+ * @date 2022-10-13 20:05:47
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
